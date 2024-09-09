494d85a661bd0bf176e1d988cfd929afbea7b85c
boola anirudh <boolaanirudh14m@gmail.com>
2024-09-09 13:18:29 +0530

    first commit

---
+++ Weather-App-start/src/main/java/com/example/weather_app/model/WeatherResponse.java
@@ -0,0 +1,49 @@
+package com.example.weather_app.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class WeatherResponse {
+    private String name;
+    private Sys sys;
+    private List<Weather> weather;
+    private Main main;
+    private Wind wind;
+
+    @Setter
+    @Getter
+    public static class Sys
+    {
+
+        private String country;
+    }
+
+    @Setter
+    @Getter
+    public  static class Weather
+    {
+
+        private int id;
+        private String description;
+    }
+    @Setter
+    @Getter
+    public static class Main
+    {
+
+        private double temp;
+
+        private int humidity;
+    }
+    @Getter
+    @Setter
+    public static class Wind
+    {
+        private double speed;
+
+    }
+}
